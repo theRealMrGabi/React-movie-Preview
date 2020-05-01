ed8be134bc37f86d6a24f4115138f2fc0e86f405
gabirated <gabirated@gmail.com>
2020-05-01 16:19:08 +0100

    js cdn link was added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route , Switch} from 'react-router-dom';
 import Homepage from './components/Homepage/Homepage';
 import Details from './components/Details/Details';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-import 'materialize-css/dist/css/materialize.min.css';
 import PopularMovie from './components/PopularMovies/PopularMovies';
 import UpcomingMovies from './components/UpcomingMovies/UpcomingMovies';
 import TrendingTv from './components/TV/TrendingTv/TrendingTv';
 import PopularTv from './components/TV/PopularTv/PopularTv';
 import TvDetails from './components/TV/TvDetails/TvDetails';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css';
 
 const App = () => {
+
+  useEffect(() => {
+    M.AutoInit();
+  }, [])
 
   return (
     <React.Fragment>
